3224cc6aea551327aaf51695d091895ed2572fa3
Студент <student@ad.anosov.ru>
2023-05-23 12:56:45 +0500

    first commit

---
+++ Pages/Auth.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace wa3213123.Pages.Shared
+{
+    public class AuthModel : PageModel
+    {
+        public void OnGet()0
+        {
+
+        }
+    }
+}
