8195c2e63eee10ab40e51cda522268118d86d641
Студент <student@ad.anosov.ru>
2023-05-23 13:51:25 +0500

    nemnogo peredelal

--- Pages/Auth.cshtml.cs
+++ Pages/Auth.cshtml.cs
@@ -5,9 +5,21 @@ namespace wa3213123.Pages.Shared
 {
     public class AuthModel : PageModel
     {
-        public void OnGet()0
+        public void OnGet()
         {
 
+        }
+        public async Task<IActionResult> OnPost(string? returnUrl) 
+        {
+            var form = HttpContext.Request.Form;
+            if (!form.ContainsKey("login") || !form.ContainsKey("password"))
+                return BadRequest("Email и/или пароль не установлены");
+
+            string email = form["login"];
+            string password = form["password"];
+
+            var db = new MaindbContext();
+            var staff = db.Staffs 
         }
     }
 }
